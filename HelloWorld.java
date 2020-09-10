f1741ef3355a055c92c0cd2614c7db3b49abda0f
poncoe <poncoe4mac@gmail.com>
2020-09-10 23:28:32 +0700

    meonk commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Nama : Arief Wardhana");
+        System.out.println("Panggilan : Poncoe");
+        System.out.println("Nim : 6706204020");
+        System.out.println("Kelas : D3IF-44-01");
+        System.out.println("");
+    }
+}
